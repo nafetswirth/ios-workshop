50570efe301bce27b366e18c5b4ade05dc6c45fa
Dat Dev <stefane@hyph.me>
2016-02-01 16:32:37 +0100

    Initial Commit

---
+++ Workshop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Workshop
+//
+//  Created by Stefan Wirth on 01/02/16.
+//  Copyright © 2016 Stefan Wirth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
