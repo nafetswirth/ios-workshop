b90ed810508d97e120a56ef1bfe0936ff4edc7ea
Dat Dev <stefane@hyph.me>
2016-03-03 14:03:49 +0100

    added socket.io client

---
+++ iOS/Workshop/SocketService.swift
@@ -0,0 +1,14 @@
+//
+//  SocketService.swift
+//  Workshop
+//
+//  Created by Stefan Wirth on 03/03/16.
+//  Copyright © 2016 Stefan Wirth. All rights reserved.
+//
+
+import Foundation
+import SocketIOClientSwift
+
+struct SocketService {
+    
+}
